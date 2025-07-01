42a22cffa3be340bd8c1cc30ed9019bdda70ca11
Andryu Schittone <139622527+MaliosDark@users.noreply.github.com>
2025-07-01 21:34:23 +0100

    Fix duplicate walletService import

--- src/components/modals/GetTestTokensModal.jsx
+++ src/components/modals/GetTestTokensModal.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import apiService from '../../services/apiService';
 import walletService from '../../services/walletService';
-import walletService from '../../services/walletService';
 import ENV from '../../config/environment';
 
 const GetTestTokensModal = ({ onClose }) => {
